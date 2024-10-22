f2f22f2bf5315578db5a3f9d88a00eb27e4bd1c0
Gias uddin vuiya <giasuddinvuiya2022@gmail.com>
2024-10-22 11:22:08 +0600

    added tip,faces,einstain exercise solutions

---
+++ Week_0/Exercises/einstain/einstain.py
@@ -0,0 +1,10 @@
+def main():
+
+    mass = int(input("m: "))
+    speed = 300000000
+
+    energy = mass * speed * speed
+    print(f"E: {energy}")
+
+
+main()
---
+++ Week_0/Exercises/faces/faces.py
@@ -0,0 +1,16 @@
+def main():
+
+    convert = input("")
+
+    if convert == "hello :)":
+        print("hello 🙂")
+    elif convert == "Hello :)":
+        print("Hello 🙂")
+    elif convert == "goodbye :(":
+        print("goodbye 🙁")
+    elif convert == "Goodbye :(":
+         print("Goodbye 🙁")
+    elif convert == "Hello :) Goodbye :(":
+        print("Hello 🙂 Goodbye 🙁")
+
+main()
